f8f03320ae8b864f29a0ca3473bd2c9bb383675d
Sreekar Venkata Nutulapati <sreekarnv1@gmail.com>
2021-10-13 12:15:15 +0530

    fixed pagination bug

--- app/src/config/apollo.ts
+++ app/src/config/apollo.ts
@@ -1,6 +1,27 @@
 import { SERVER_URL } from '@env';
 import { createUploadLink } from 'apollo-upload-client';
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { PostsResponse } from '../graphql/generated';
+
+const mergePosts = (existing: any, incoming: any) => {
+	let updated: PostsResponse = {
+		__typename: 'PostsResponse',
+		hasNext: true,
+		posts: [],
+	};
+
+	if (!existing?.posts?.length) {
+		return incoming;
+	}
+
+	if (existing?.posts[0].__ref === incoming?.posts[0].__ref) {
+		return existing;
+	}
+
+	updated.hasNext = incoming.hasNext;
+	updated.posts = [...existing.posts, ...incoming.posts];
+	return updated;
+};
 
 const apolloClient = new ApolloClient({
 	cache: new InMemoryCache({
@@ -17,23 +38,22 @@ const apolloClient = new ApolloClient({
 							},
 							incoming
 						) {
-							let updated: any = {
+							const updatedPosts = mergePosts(existing, incoming);
+							return updatedPosts;
+						},
+					},
+					getUserPosts: {
+						keyArgs: false,
+						merge(
+							existing = {
 								__typename: 'PostsResponse',
 								hasNext: true,
 								posts: [],
-							};
-
-							if (!existing?.posts?.length) {
-								return incoming;
-							}
-
-							if (existing?.posts[0].__ref === incoming?.posts[0].__ref) {
-								return existing;
-							}
-
-							updated.hasNext = incoming.hasNext;
-							updated.posts = [...existing.posts, ...incoming.posts];
-							return updated;
+							},
+							incoming
+						) {
+							const updatedPosts = mergePosts(existing, incoming);
+							return updatedPosts;
 						},
 					},
 				},
